881ece9d8976a3604515366940b46d699d763d2a
sam-mantey <98030457+sam-mantey@users.noreply.github.com>
2023-09-28 11:22:11 +0000

    added the navlinks

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import Logo from '../assets/Logo.svg'
+import { Link,NavLink } from 'react-router-dom'
 
 
 export default function Nav() {
   return (
     <> 
-        <nav>
-          <div><a><img src="../assets/Logo.svg" alt="Logo" /></a></div>
+        <nav className='flex items-center justify-between w-[80%] bg-[#e8e9e9] mx-auto px-[20px] py-[10px]'>
+          <div>
+            <NavLink>
+              <img src={Logo} alt="Logo" />
+            </NavLink>
+            </div>
+          <div>
+            <ul className='flex items-center gap-4'> 
+              <li><NavLink>Home</NavLink></li>
+              <li><NavLink>About</NavLink></li>
+              <li><NavLink>Menu</NavLink></li>
+              <li><NavLink>Reservations</NavLink></li>
+              <li><NavLink>Order Online</NavLink></li>
+              <li><NavLink>Log In</NavLink></li>
+            </ul>
+          </div>
+          <div>
+            <img src="" alt="" />
+          </div>
+          
         </nav>
+        
     </>
   )
 }
